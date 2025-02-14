27a931f752cfa4c038d4d9052a8b0732cdf5030a
Techatach <techatach@gmail.com>
2025-02-14 23:38:42 +0700

    03-create-website-layout

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,24 @@
+import { ShoppingCartIcon, UserIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Menu() {
+  return (
+    <div className="flex justify-end">
+      <nav className="flex gap-3 w-full">
+        <Link href="/signin" className="flex items-center header-button">
+          {/* <UserIcon className="h-8 w-8" /> */}
+          {/* <span className="font-bold">Sign in</span> */}
+          Hellow, Sign in
+        </Link>
+
+        <Link href="/cart" className="header-button">
+          <div className="flex items-end">
+            <ShoppingCartIcon className="h-8 w-8" />
+            {/* <div className="font-bold">Cart</div> */}
+            Cart
+          </div>
+        </Link>
+      </nav>
+    </div>
+  );
+}
